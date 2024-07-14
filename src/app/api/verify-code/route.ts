b88a2369c4bd261aa15b91321731a9b246215615
aryan <aryanpandit1718@gmail.com>
2024-07-14 14:18:44 +0530

    changes 14:18

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -36,6 +36,7 @@ export async function POST(request : NextRequest){
             })
         }
         user.isVerify = true
+        user.verifyCode = ''
 
         await user.save()
         return NextResponse.json({
