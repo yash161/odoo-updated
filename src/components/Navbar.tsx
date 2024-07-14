afb13573b1966a8f01fff1a65caf01789a2e4701
aryan <aryanpandit1718@gmail.com>
2024-07-14 15:10:02 +0530

    changes15:09

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,14 +53,6 @@ function Navbar({ className }: { className?: string }) {
         </li>
         <li>
           <Link
-            href="/contact"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link
             href="/login"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
@@ -69,7 +61,7 @@ function Navbar({ className }: { className?: string }) {
         </li>
         <li>
           <Link
-            href="/signUp"
+            href="/sign-up"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
             Sign Up
